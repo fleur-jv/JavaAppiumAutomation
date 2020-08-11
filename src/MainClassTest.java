9cbb27c44b2dfbab37f2f5f1cd2a20d4bb070b44
Yuliya Volosatova <yuliya.volosatova@vseinstrumenti.ru>
2020-08-11 22:30:15 +0300

    add test 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -11,4 +11,14 @@ public class MainClassTest
         int result = main.getLocalNumber();
         Assert.assertEquals("Метод возвращает не 14", 14, result);
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        MainClass main = new MainClass();
+
+        int result = main.getClassNumber();
+        int actualClassValue = 45;
+        Assert.assertTrue("Метод возвращает число меньше 45", result > actualClassValue);
+    }
 }
